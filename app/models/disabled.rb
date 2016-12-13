a70470cd648b89f404b573e141ae802e705831e0
DausdasKreuz <dausdaskreuz@gmail.com>
2016-12-13 17:05:57 +0100

    Seeds with real pictograms in images

--- app/models/disabled.rb
+++ app/models/disabled.rb
@@ -1,6 +1,6 @@
 class Disabled < ApplicationRecord
-  belongs_to :professional, class_name: 'User'#, foreign_key: 'professional_id'
-  has_one :guide, class_name: 'User'#, foreign_key: 'guide_id'
+  belongs_to :professional, class_name: 'User'
+  has_one :guide, class_name: 'User'
   has_many :agendas
 
   validates :name, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,62 +7,139 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'ffaker'
 
-# Create professionals
-users = User.create([
-  {name: 'Dani Garcia',     email: 'dani@bmv.io',  role: 'professional', password: 'secret', password_confirmation: 'secret'},
-  {name: 'Ana Toscano',     email: 'ana@bmv.io',   role: 'professional', password: 'secret', password_confirmation: 'secret'},
-  {name: 'Jose Toscano',    email: 'jose@bmv.io',  role: 'professional', password: 'secret', password_confirmation: 'secret'},
-  {name: 'Harec Silvered',  email: 'harec@bmv.io', role: 'professional', password: 'secret', password_confirmation: 'secret'},
-  {name: 'Alfonso Huescar', email: 'alfon@bmv.io', role: 'professional', password: 'secret', password_confirmation: 'secret'},
-  {name: 'Alex Ugena',      email: 'alex@bmv.io',  role: 'professional', password: 'secret', password_confirmation: 'secret'}
-])
 
-10.times do |p|
-  Pictogram.create(
-  name:     "Pictogram " + p.to_s,
-  image:    FFaker::Avatar.image,
+# Create professionals
+professional_1 = ['Dani Garcia', 'dani@bmv.io', 'professional', 'secret', 'secret']
+professional_2 = ['Ana Toscano', 'ana@bmv.io', 'professional', 'secret', 'secret']
+professional_container = [professional_1, professional_2]
+
+users = []
+professional_container.each do |professional|
+  user = User.create(
+    name:                   professional[0],
+    email:                  professional[1],
+    role:                   professional[2],
+    password:               professional[3],
+    password_confirmation:  professional[4]
   )
+  users.push(user)
 end
 
 
-# Create disableds
+# Creating disableds
+childs = ['Pablo', 'Lidia']
+adults = ['Daniel', 'Inma']
+disableds_container = [childs, adults]
+
 disableds = []
-users.each do |user|
-  3.times do
-    disabled = Disabled.create(
-      name:             FFaker::Name.first_name + " Disabled",
-      professional_id:  user.id
-    )
-    disableds.push(disabled)
+disableds_container.each do |disabled_type|
+  users.each do |professional|
+    disabled_type.each do |person|
+      disabled = Disabled.create(
+        name:             person,
+        professional_id:  professional.id
+      )
+      disableds.push(disabled)
+
+      # Creating agendas & boards
+      if disabled_type == childs
+        house_child_agenda = disabled.agendas.create(
+          name: 'Estoy en casa',
+          image: 'child_class_pictograms/casa_1.png'
+        )
+        school_agenda = disabled.agendas.create(
+          name: 'Estoy en el colegio',
+          image: 'child_house_pictograms/colegio.png'
+        )
+
+        house_child_agenda.boards.create(
+          name: 'Este soy yo',
+          image: 'meeting_pictograms/hola.png'
+        )
+        house_child_agenda.boards.create(
+          name: 'Qué puedo hacer en casa',
+          image: 'child_class_pictograms/casa.png'
+        )
+
+        school_agenda.boards.create(
+          name: 'Este soy yo',
+          image: 'meeting_pictograms/hola.png'
+        )
+        school_agenda.boards.create(
+          name: 'Que puedo hacer en el colegio',
+          image: 'child_school_pictograms/colegio.png'
+        )
+      else
+        adult_house_agenda = disabled.agendas.create(
+          name: 'Rutinas en casa',
+          image: 'adult_house_pictograms/casa.png'
+        )
+
+        adult_house_agenda.boards.create(
+          name: 'Presentación',
+          image: 'meeting_pictograms/hola.png'
+        )
+        adult_house_agenda.boards.create(
+          name: 'Ver la tele',
+          image: 'adult_house_pictograms/televión.png'
+        )
+      end
+    end
   end
 end
 
+
+# Creating guides
 disableds.each do |disabled|
   guide = User.create(
-    name:                   FFaker::Name.first_name + " Guide",
-    email:                  FFaker::Internet.safe_email,
-    password:               'secret',
-    password_confirmation:  'secret',
-    role:                   "guide"
+    name: FFaker::Name.first_name,
+    email: FFaker::Internet.safe_email,
+    role: 'guide',
+    password: 'secret',
+    password_confirmation: 'secret'
   )
-  disabled.update(guide_id: guide.id)
+  disabled.guide_id = guide.id
+end
 
-  2.times do
-    agenda = Agenda.create(
-    name:         "Agenda " + disabled.id.to_s,
-    image:        FFaker::Avatar.image,
-    disabled_id:  disabled.id
+
+# Filling boards with pictograms
+adult_house_pictograms = ['casa', 'televisión']
+child_house_pictograms = ['abuelos', 'aburrido', 'baño', 'beber', 'calle',
+  'cansado', 'centro_comercial', 'colegio', 'comer_2', 'contento_1', 'cuento',
+  'dormir_1', 'enfadado', 'hermana', 'ir', 'jugar', 'mamá', 'música_1', 'no_1',
+  'ordenandor_1', 'papá', 'parque', 'primo', 'sí_1', 'triste_1', 'yo']
+child_class_pictograms = ['amigos', 'autobús', 'baño', 'beber', 'casa_1',
+  'clase_6', 'comer_2', 'contento_1', 'enfadado', 'ir', 'logopeda', 'música_1',
+  'no_1', 'ordenador_1', 'patio', 'profesora', 'querer', 'sí_1' ,'triste_1',
+  'yo']
+meeting_pictograms = ['adiós', 'colegio', 'hermana', 'hola', 'mamá', 'no_1',
+  'papá', 'sí_1', 'yo']
+
+
+boards = Board.all
+boards.each do |board|
+  container = case board.name
+    when 'Este soy yo' then meeting_pictograms
+    when 'Presentación' then meeting_pictograms
+    when 'Qué puedo hacer en casa' then child_house_pictograms
+    when 'Que puedo hacer en el colegio' then child_class_pictograms
+    when 'Ver la tele' then adult_house_pictograms
+  end
+  path = case board.name
+    when 'Este soy yo' then 'meeting_pictograms'
+    when 'Presentación' then 'meeting_pictograms'
+    when 'Qué puedo hacer en casa' then 'child_house_pictograms'
+    when 'Que puedo hacer en el colegio' then 'child_class_pictograms'
+    when 'Ver la tele' then 'adult_house_pictograms'
+  end
+  if !container
+    binding.pry
+  end
+  container.each do |image|
+    pictogram = Pictogram.create(
+      name:  image,
+      image: path + '/' + image.to_s + '.png'
     )
-
-    2.times do |i|
-      board = Board.create(
-      name:       "Board " + i.to_s,
-      image:      FFaker::Avatar.image,
-      agenda_id:  agenda.id
-      )
-      for time in 1..10
-        board.pictograms.push(Pictogram.find(time))
-      end
-    end
+    board.pictograms << pictogram
   end
 end
