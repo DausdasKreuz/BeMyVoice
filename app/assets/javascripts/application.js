362b2f5c7647c2bcb9edad45b58686ee36ec2f10
DausdasKreuz <dausdaskreuz@gmail.com>
2016-12-14 19:33:05 +0100

    js functions added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,49 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var picList = [];
+
+function renderPicList() {
+  // clean old list
+  var message = document.querySelector('.message-content');
+  message.innerHTML = '';
+
+  // iterate and render
+  for (var pic of picList) {
+    picURL = pic.attributes.src.value;
+      message.innerHTML += `<div class="message-card"><img src="${picURL}" /><i class="material-icons close">clear</i></div>`;
+  }
+
+  // bind all close buttons with their functions
+  document.querySelectorAll('.material-icons.close').forEach(function(closeButton, index) {
+      closeButton.onclick = removeFromList.bind(null, index);
+  });
+}
+
+function addToList(image) {
+  console.log("Añadiendo to list la siguiente imagen", image);
+  picList.push(image.children[0]);
+  renderPicList();
+}
+
+function removeFromList(index) {
+  console.log("Removiendo to list la imagen con index", index);
+  picList.splice(index, 1);
+  renderPicList();
+}
+
+function clearList() {
+  picList = [];
+  renderPicList();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+console.log('LOADED');
+// Configure all images to be interactive
+document.querySelectorAll('.card-image').forEach(function(image) {
+    image.onclick = addToList.bind(null, image);
+});
+
+document.querySelector('.material-icons.done').onclick = function() { clearList() };
+});
